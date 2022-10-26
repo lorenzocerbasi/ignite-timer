12292ba3aa409bba9017c2f0bd9adcda7b8f9c67
Lorenzo Cerbasi <lorenzocerbasi1@hotmail.com>
2022-10-26 14:01:25 -0300

    fix:Arrumando o layout do Timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,7 +5,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 
 const createUserFormSchema = yup.object({
   task: yup.string().required("É necessário informar uma tarefa!"),
-  time: yup.number().min(5).max(60).required("É necessário informar um tempo!")
+  time: yup.number().min(5, "O tempo precisar maior que 5 minutos!").max(60, "O tempo precisar menor que 60 minutos!").required("É necessário informar um tempo!")
 })
 
 export function Timer() {
@@ -25,28 +25,30 @@ export function Timer() {
   return (
     <>
       <div className='flex flex-col items-center mt-10 max-w-2xl mx-auto'>
-        <form onSubmit={handleSubmit(createTask)} className='flex flex-col text-gray7 mb-[100px] items-center w-full'>
-          <label htmlFor='task' className='font-bold text-lg mr-2'>Vou trabalhar em</label>
-          <input
-            id='task'
-            placeholder="Dê um nome para o seu projeto"
-            className="focus:outline-none text-gray4 bg-transparent h-10 font-bold text-lg px-2 border-b-2 border-gray4 flex-1 text-center no-arrow"
-            list='task-suggestion'
-            {...register('task')}
-          />
-          <datalist id="task-suggestion">
-            <option>Teste</option>
-          </datalist>
-          <label htmlFor='time' className='mx-2'>durante</label>
-          <input
-            id='time'
-            placeholder="0"
-            type="number"
-            className="focus:outline-none text-gray4 bg-transparent h-10 w-16 border-b-2 border-gray4 text-center"
-            step={5} min={5} max={60}
-            {...register('time', { valueAsNumber: true })}
-          />
-          <label htmlFor='time' className='ml-2'>minutos.</label>
+        <form onSubmit={handleSubmit(createTask)} className='flex flex-col text-gray7 items-center w-full'>
+          <div className="flex mb-[100px]">
+            <label htmlFor='task' className='font-bold text-lg mr-2'>Vou trabalhar em</label>
+            <input
+              id='task'
+              placeholder="Dê um nome para o seu projeto"
+              className="focus:outline-none text-gray4 bg-transparent h-10 font-bold text-lg px-2 border-b-2 border-gray4 flex-1 text-center no-arrow"
+              list='task-suggestion'
+              {...register('task')}
+            />
+            <datalist id="task-suggestion">
+              <option>Teste</option>
+            </datalist>
+            <label htmlFor='time' className='mx-2'>durante</label>
+            <input
+              id='time'
+              placeholder="0"
+              type="number"
+              className="focus:outline-none text-gray4 bg-transparent h-10 w-16 border-b-2 border-gray4 text-center"
+              step={5} min={5} max={60}
+              {...register('time', { valueAsNumber: true })}
+            />
+            <label htmlFor='time' className='ml-2'>minutos.</label>
+          </div>
           <div className='mb-14 font-coutdown text-[10rem] leading-[8rem] text-gray7 font-bold select-none'>
             <span className='bg-gray-700 px-6 py-4 mr-4 rounded-lg'>0</span>
             <span className='bg-gray-700 px-6 py-4 rounded-lg'>0</span>
