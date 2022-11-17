24e94347d62e67b0f18ec80188855b125f6970f4
Lorenzo Cerbasi <lorenzocerbasi1@hotmail.com>
2022-11-17 19:43:13 -0300

    update: new version

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,22 @@
-import { Play } from "phosphor-react";
+import { Play, HandPalm } from "phosphor-react";
 import { useForm } from "react-hook-form"
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns"
 
 interface TypesFormTimer {
   task: string,
-  time: number
+  time: number,
+}
+
+interface Cycle {
+  id: string,
+  task: string,
+  time: number,
+  startDate: Date,
+  interruptedDate?: Date,
+  finishedDate?: Date,
 }
 
 const createUserFormSchema = yup.object({
@@ -14,6 +25,11 @@ const createUserFormSchema = yup.object({
 })
 
 export function Timer() {
+
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [secondPassed, setSecondPassed] = useState(0)
+
   const { register, handleSubmit, watch, formState: { errors }, reset } = useForm<TypesFormTimer>({
     resolver: yupResolver(createUserFormSchema),
     defaultValues: {
@@ -22,9 +38,74 @@ export function Timer() {
     }
   });
 
+  const activeCycle = cycles?.find((i: any) => i?.id === activeCycleId)
+
+  const convertMinuteToSecond = activeCycle ? activeCycle?.time * 60 : 0
+  const totalSeconds = convertMinuteToSecond
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(new Date(), activeCycle?.startDate)
+
+        if (secondsDifference >= totalSeconds) {
+          setCycles(state => state?.map((cycle) => {
+            if (cycle?.id === activeCycleId) {
+              return { ...cycle, finishedDate: new Date() }
+            } else {
+              return cycle
+            }
+          }))
+          setSecondPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setSecondPassed(secondsDifference)
+        }
+      }, 1000)
+    }
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle, totalSeconds])
+
   function createTask(data: TypesFormTimer) {
-    console.log(data)
+    const id = new Date().getTime().toString()
+    const newCycle: Cycle = {
+      id,
+      task: data?.task,
+      time: data?.time,
+      startDate: new Date(),
+    }
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setSecondPassed(0)
   }
+
+  function interruptCycle() {
+    setCycles(state => state?.map((cycle) => {
+      if (cycle?.id === activeCycleId) {
+        return { ...cycle, interruptedDate: new Date() }
+      } else {
+        return cycle
+      }
+    }))
+    setActiveCycleId(null)
+  }
+  const currentSeconds = activeCycle ? totalSeconds - secondPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds])
 
   const task = watch('task')
   const buttonDisabled = !task
@@ -39,8 +120,9 @@ export function Timer() {
               <input
                 id='task'
                 placeholder="Dê um nome para o seu projeto"
-                className={`focus:outline-none text-gray4 bg-transparent h-10 font-bold text-lg px-2 border-b-2 flex-1 text-center no-arrow ${errors?.task ? "border-red" : "border-gray4" }`}
+                className={`focus:outline-none text-gray4 bg-transparent h-10 font-bold text-lg px-2 border-b-2 flex-1 text-center no-arrow ${errors?.task ? "border-red" : "border-gray4"}`}
                 list='task-suggestion'
+                disabled={!!activeCycle}
                 {...register('task')}
               />
             </div>
@@ -52,26 +134,36 @@ export function Timer() {
               id='time'
               placeholder="0"
               type="number"
-              className={`focus:outline-none text-gray4 bg-transparent h-10 w-16 border-b-2 text-center ${errors?.time ? "border-red" : "border-gray4" }`}
+              className={`focus:outline-none text-gray4 bg-transparent h-10 w-16 border-b-2 text-center ${errors?.time ? "border-red" : "border-gray4"}`}
               min={5} max={60}
+              disabled={!!activeCycle}
               {...register('time', { valueAsNumber: true })}
             />
             <label htmlFor='time' className='ml-2'>minutos.</label>
           </div>
           <div className='mb-14 font-coutdown text-[10rem] leading-[8rem] text-gray7 font-bold select-none'>
-            <span className='bg-gray-700 px-6 py-4 mr-4 rounded-lg'>0</span>
-            <span className='bg-gray-700 px-6 py-4 rounded-lg'>0</span>
+            <span className='bg-gray-700 px-6 py-4 mr-4 rounded-lg'>{minutes[0]}</span>
+            <span className='bg-gray-700 px-6 py-4 rounded-lg'>{minutes[1]}</span>
             <span className='text-green'>:</span>
-            <span className='bg-gray-700 px-6 py-4 mr-4 rounded-lg'>0</span>
-            <span className='bg-gray-700 px-6 py-4 rounded-lg'>0</span>
+            <span className='bg-gray-700 px-6 py-4 mr-4 rounded-lg'>{seconds[0]}</span>
+            <span className='bg-gray-700 px-6 py-4 rounded-lg'>{seconds[1]}</span>
           </div>
-          <button
-            className='bg-green text-white w-full py-5 rounded-lg flex items-center justify-center mb-20 mt-14 transition-all disabled:bg-green disabled:opacity-70 hover:bg-green-dark '
-            disabled={buttonDisabled}
-          >
-            <Play size={30} className='mr-1' />
-            Começar
-          </button>
+          {!activeCycle
+            ? <button
+              className='bg-green text-white w-full py-5 rounded-lg flex items-center justify-center mb-20 mt-14 transition-all disabled:bg-green disabled:opacity-70 hover:bg-green-dark '
+              disabled={buttonDisabled}
+            >
+              <Play size={30} className='mr-1' />
+              Começar
+            </button>
+            : <button
+              className='bg-red text-white w-full py-5 rounded-lg flex items-center justify-center mb-20 mt-14 transition-all disabled:opacity-70 hover:bg-red-dark '
+              type="button" onClick={interruptCycle}
+            >
+              <HandPalm size={30} className='mr-1' />
+              Interromper
+            </button>
+          }
         </form>
       </div>
     </>
